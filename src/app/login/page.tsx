5e89c9909e2af43c46e996d404ce3930e59c2744
MahmoudHamid25 <mahmoud.hamid@student.nhlstenden.com>
2024-05-27 22:29:48 +0200

    Add: Login form V1

---
+++ src/app/login/page.tsx
@@ -0,0 +1,18 @@
+function LoginForm() {
+    return (
+      <div className={"loginForm"}>
+        <h1>Sign in to StudyHub</h1>
+        <form>
+          <p/><label htmlFor={"email"}>E-mail Address</label>
+          <p/><input type={"email"} name={"email"} />
+          <p/><label htmlFor={"password"}>Password</label>
+          <p/><input type={"password"} name={"password"} />
+          <p/><a href={"/forgot-password"}>Forgot your password?</a>
+        </form>
+        <button className={"indexButton"}><a href={""}>Sign in</a></button>
+      </div>
+    );
+  }
+  
+  export default LoginForm;
+
